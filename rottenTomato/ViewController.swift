e9c0d0d21e01fa04bd549d443dfcfb32a1b7a36a
Linghua Jin <ljin@walmartlabs.com>
2015-09-14 00:22:27 -0800

    added pull to refresh

--- rottenTomato/ViewController.swift
+++ rottenTomato/ViewController.swift
@@ -12,15 +12,14 @@ private let API_ROOT = "http://localhost:8000/"
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var movies:NSArray?
+//    create the UIRefreshControl as an instance variable at the top of the class because you need to access it to stop the loading behavior.
+    var refreshControl: UIRefreshControl!
+    
     //movieList is the uitable outlet
     @IBOutlet weak var movieList: UITableView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        movieList.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
-        //TODO: switch with remote uri
-        //let cachedDataUrlString = "http://localhost:8000/"
+    func makeRequest(closure:()->()){
+         //TODO: switch with remote uri
         let request = NSMutableURLRequest(URL: NSURL(string:API_ROOT)!)
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request){
             (data, response, error) -> Void in
@@ -31,9 +30,42 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                     self.movies = dictionary["movies"] as? NSArray
                     self.movieList.reloadData()
                 }
+                NSLog("request complete, dispatch to the main queue")
+                closure()
             }
         }
         task.resume()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //otherwise didSelectRowAtIndexPath won't work
+        movieList.delegate = self
+        makeRequest({});
+       
+//        add the refresh control as a subview of the scrollview. It's best to insert it at the lowest index so that it appears behind all the views in the scrollview. 
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+         movieList.insertSubview(refreshControl, atIndex: 0)
+    }
+// DO NOT DELETE
+// REFERENCE FOR swift closure
+//    func delay(delay:Double, closure:()->()) {
+//        dispatch_after(
+//            dispatch_time(
+//                DISPATCH_TIME_NOW,
+//                Int64(delay * Double(NSEC_PER_SEC))
+//            ),
+//            dispatch_get_main_queue(), closure)
+//    }
+    
+    func onRefresh() {
+         makeRequest({
+            self.refreshControl.endRefreshing()
+         });
+//        delay(2, closure: {
+//            self.refreshControl.endRefreshing()
+//        })
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
